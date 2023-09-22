8604afeb7fd41a158c3fd41f0a4ebac552e60f81
Vishal <potula.vishal10@gmail.com>
2023-09-23 00:47:58 +0530

    display x when square is clicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import Board from './components/Board'
+import { useState } from 'react';
+import Board from './components/Board';
 import './style.scss';
 
 function App() {
+  // const [counter, setcounter] = useState(0);
+
+  // console.log('hello signore');
+
+  // const onbtnclick = () => {
+  //   setcounter(currentcounter => {
+  //     return currentcounter + 1;
+  //   });
+  // };
+  // let counter = 1;
+
   return (
-    <div className='app'>
+    <div className="app">
+      <div>
       <Board/>
+
+        {/* <button onClick={onbtnclick}>click here to increment</button> */}
+        {/* <div>{counter}</div> */}
+
+      </div>
     </div>
-      
-        
-    
   );
 }
 
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,22 +1,44 @@
+import { useState } from 'react';
 import Square from './Square';
 
 const Board = () => {
+  const [squares, setsquares] = useState(Array(9).fill(null));
+
+  console.log(squares)
+
+  const handlesquareclick = clickedposition => {
+    setsquares(currentsquares =>{
+      return currentsquares.map((squarevalue, position)=>{
+        if(clickedposition === position){
+        return "x";
+        }
+      return squarevalue;
+      })
+    })
+  };
+
+  const rendersquare = position => {
+    return (
+      <Square value={squares[position]} onClick={()=> handlesquareclick(position)} />
+    );
+  };
+
   return (
     <div className="board">
       <div className="board-row">
-        <Square value={0}/>
-        <Square value={1}/>
-        <Square value={2}/>
+        {rendersquare(0)}
+        {rendersquare(1)}
+        {rendersquare(2)}
       </div>
       <div className="board-row">
-        <Square value={3}/>
-        <Square value={4}/>
-        <Square value={5}/>
+        {rendersquare(3)}
+        {rendersquare(4)}
+        {rendersquare(5)}
       </div>
       <div className="board-row">
-        <Square value={6}/>
-        <Square value={7}/>
-        <Square value={8}/>
+        {rendersquare(6)}
+        {rendersquare(7)}
+        {rendersquare(8)}
       </div>
     </div>
   );
